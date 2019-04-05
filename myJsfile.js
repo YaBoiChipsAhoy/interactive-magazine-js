70b1de392160d2bbfa333e77dcc1329db56092a0
Levels Teen Center <levelsteencenter@Levelss-MacBook-Pro.local>
2019-04-05 18:19:00 -0400

    "
    i hello

--- myJsfile.js
+++ myJsfile.js
@@ -4,3 +4,12 @@ document.getElementById("demo").innerHTML = "This is my new text";
 
 document.getElementById('demo').style.color = "blue"
 "blue"
+
+function usingVariables(){
+ var userName = prompt("what is your name?")
+	var textElement = document.getElementById("text")
+	textElement.innerHTML = "Hi  " + userName
+  var header = document.getElementById("head")
+	header.innerHTML = header.innerHTML + " By " + userName
+
+}
